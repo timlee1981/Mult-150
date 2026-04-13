45c1f7b10fbf7513c0c31b8480031d07e2662995
Timothy Lee <timlee1981@gmail.com>
2026-04-12 22:11:03 -0700

    Hour 8 and 21
    Hour 8 and 21

---
+++ tlee_Hour8and21/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(Input.GetButtonDown("Jump"))
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            if(audioSource.isPlaying == true)
+            {
+                audioSource.Stop();
+            }
+            else
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
